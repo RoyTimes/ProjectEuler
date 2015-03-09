0df567714f0b2b606578bd19cdb34afe0c259182
Zhou Song <roytimes@icloud.com>
2015-03-09 17:23:18 +0800

    Solve the problem 59 and did some delete work

--- 59.cpp
+++ 59.cpp
@@ -5,29 +5,39 @@
 #include <algorithm>
 using namespace std;
 
-const int N = 100010;
-const int In [N];
-int tot = 0;
-
-
-string Crype (int a, int b, int c){
-	for (int i = 0; i < tot; i ++)
-}
-
+const int N = 3000;
+int In [N], tot = 0, ans = 0;
 int main()
 {
-	freopen ("data.in", "r",stdin);
-	ios::sync_with_stdio(false);
-	
-	while (cin >> In[tot ++]);
+    FILE *f = fopen("data.in", "r");
+    for (char c = fgetc(f); c != EOF; c = fgetc(f))
+        if (c != ',')
+            In [tot] = In [tot] * 10 + c - '0';
+        else tot ++;
+    fclose(f);
 
-	for (int i = 'a'; i <= 'z'; i ++)
-		for (int j = 'a'; j <= 'z'; j ++)
-			for (int k = 'a'; k <= 'z'; k ++){
-				string get = Crype (i, j, k);
-				if (Judge (get)){
-					cout << GetSum (get) << endl;
-					return 0;
-				}
-			}
+    int max_sum, max_alpha, temp_alpha, temp_sum;
+    max_sum = max_alpha = temp_sum = temp_alpha = 0;
+
+    for (int cut = 0; cut < 3; cut ++)
+    {
+        max_sum = max_alpha = 0;
+        for (char c = 'a'; c <= 'z'; c ++)
+        {
+            temp_alpha = temp_sum = 0;
+            for (int j = cut; j < tot; j += 3)
+            {
+                char temp = In [j] ^ c;
+                if (temp <= 'z' && temp >= 'a')
+                    temp_alpha ++;
+                temp_sum += temp;
+            }
+            if (temp_alpha > max_alpha)
+                max_alpha = temp_alpha,
+                max_sum = temp_sum;
+        }
+        ans += max_sum;
+    }
+    cout << ans + '.' << endl;
+    return 0;
 }
